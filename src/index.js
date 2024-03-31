e0ec475ba754c9ceff64cd4b0af8213479c694c3
Bart <bartkof91@gmail.com>
2024-03-31 21:43:43 +0200

    Initial Commit - project structure

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./js/main.js";
+import "./scss/styles.scss";
+
+console.log("hello world");
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js", // Plik główny
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: { directory: path.join(__dirname, "dist") },
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/, // Rozpoznaje pliki .scss
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // Szablon HTML
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css", // Nazwa wyjściowego pliku CSS
+    }),
+  ],
+};
